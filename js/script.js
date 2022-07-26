5dfe612e2bfe529bd8a3b935cc9740d0509780e7
Kaiser69 <pen1921@me.com>
2022-07-27 01:15:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Dilbilimci ", "Mütercim", "Yabancı Dil Öğretmeni", " ( polyglot )  "],
+        strings: ["Dilbilimci ", "Mütercim", "Dil Öğretmeni", " ( polyglot )  "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
